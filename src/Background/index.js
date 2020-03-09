5d9d13e8ab7d47d0cf33a7def93d2dbd6e7c7316
Bjorn Stromberg <bjorn@bjornstar.com>
2020-03-10 00:04:29 +0900

    Move face-api usage into background page

--- src/Background/index.js
+++ src/Background/index.js
@@ -1,20 +1,64 @@
+import {
+  detectAllFaces,
+  loadFaceLandmarkModel,
+  loadFaceRecognitionModel,
+  loadSsdMobilenetv1Model,
+  SsdMobilenetv1Options
+} from 'face-api.js';
 import browser from 'webextension-polyfill';
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === 'img_to_dataurl') {
-    const img = new Image();
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = request.width;
-      canvas.height = request.height;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, request.width, request.height);
-      sendResponse({ data: canvas.toDataURL() });
-    };
-    img.width = request.width;
-    img.height = request.height;
-    img.src = request.url;
+const minConfidence = 0.7;
 
-    return true;
+const FaceLandmarkModel = 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/0.22.1/weights/face_landmark_68_model-weights_manifest.json';
+const FaceRecognitionModel = 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/0.22.1/weights/face_recognition_model-weights_manifest.json';
+const Mobilenetv1Model = 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/0.22.1/weights/ssd_mobilenetv1_model-weights_manifest.json';
+
+let modelLoaded = false;
+let modelLoading = null;
+
+function loadModel() {
+  if (modelLoaded) return;
+
+  modelLoading = modelLoading || Promise.all([
+    loadFaceLandmarkModel(FaceLandmarkModel),
+    loadFaceRecognitionModel(FaceRecognitionModel),
+    loadSsdMobilenetv1Model(Mobilenetv1Model, new SsdMobilenetv1Options({ minConfidence }))
+  ]).then(() => {
+    console.log('model loaded...');
+    modelLoaded = true;
+  }).catch(error => {
+    modelLoading = null;
+    console.error(error);
+    throw error;
+  });
+
+  return modelLoading;
+}
+
+function loadImage({ height, url, width }) {
+  return new Promise((resolve, reject) => {
+    const img = new Image(width, height);
+
+    img.onerror = reject;
+    img.onload = () => {
+      return resolve(img);
+    };
+
+    img.src = url;
+  });
+}
+
+browser.runtime.onMessage.addListener(({ height, message, url, width }, sender) => {
+  if (message === 'detect_faces') {
+    return Promise.all([loadImage({ height, url, width }), loadModel()]).then(([img]) => {
+      return detectAllFaces(img).withFaceLandmarks();
+    }).then(faces => {
+      return faces.map(({ landmarks }) => ({
+        jawOutline: landmarks.getJawOutline().map(({ x, y }) => ({ x, y })),
+        nose: landmarks.getNose().map(({ x, y }) => ({ x, y }))
+      }));
+    });
   }
 });
+
+loadModel();
